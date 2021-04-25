37830a6d769b99d7e368c7417d498e8cb0bdc479
ArvidHolmqvist <arvidholmqvist98@gmail.com>
2021-04-25 21:51:45 +0200

    Update BookingController.java

--- src/main/java/se/chalmers/TDA257/booking/BookingController.java
+++ src/main/java/se/chalmers/TDA257/booking/BookingController.java
@@ -10,7 +10,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://hamncafe-test.herokuapp.com")
+@CrossOrigin(origins = "https://hamncafe-test.herokuapp.com")
 //@CrossOrigin(origins = "http://localhost:3000")
 public class BookingController {
 
